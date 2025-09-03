434886aecadde4f8eb1f7a1d4e7dd77f34a20f7d
sai hein <saiheinthetaung669@gmail.com>
2025-09-03 16:55:44 +0630

    feat:create crud employee rest api with mysql

---
+++ src/main/java/com/sai/springboot/cruddemo/dao/EmployeeDAO.java
@@ -0,0 +1,11 @@
+package com.sai.springboot.cruddemo.dao;
+
+import com.sai.springboot.cruddemo.entity.Employee;
+import java.util.List;
+public interface EmployeeDAO {
+
+    List<Employee> findAll();
+    Employee findById(int id);
+    Employee save(Employee employee);
+    void deleteById(int theId);
+}
